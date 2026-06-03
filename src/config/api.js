930a69e8f4fae773796dcc8b439e865990d16b87
David Ellams <davidellams@hotmail.com>
2026-06-03 03:32:34 -0500

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || "https://oasisapionode-hseudrexdvbhenhv.canadacentral-01.azurewebsites.net/api";//"https://api.oasisweb4.one/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://api.oasisweb4.one/api"; //"https://oasisapionode-hseudrexdvbhenhv.canadacentral-01.azurewebsites.net/api";
 
 export default API_BASE_URL;
 
