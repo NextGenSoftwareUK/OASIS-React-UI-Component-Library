80b173796ba155834025ef06e9ee750e286234aa
David Ellams <davidellams@hotmail.com>
2026-07-08 01:47:35 -0500

    Rename package to @oasisomniverse/react, add barrel index and npmignore

---
+++ src/components/index.js
@@ -0,0 +1,26 @@
+// @oasisomniverse/react — OASIS Omniverse React components
+
+export { AvatarConnect }    from './common/AvatarConnect';
+export { Contact }          from './common/Contact';
+export { Eggs }             from './common/Eggs';
+export { Game }             from './common/Game';
+export { HyperDrive }       from './common/HyperDrive';
+export { KarmaToast }       from './common/KarmaToast';
+export { Map }              from './common/Map';
+export { MenuMessage }      from './common/MenuMessage';
+export { Messaging }        from './common/Messaging';
+export { Mission }          from './common/Mission';
+export { NavBar }           from './common/NavBar';
+export { NFT }              from './common/NFT';
+export { OApp }             from './common/OApp';
+export { OasisModal }       from './common/OasisModal';
+export { ONET }             from './common/ONET';
+export { ONODE }            from './common/ONODE';
+export { ProviderDropdown } from './common/ProviderDropdown';
+export { Providers }        from './common/Providers';
+export { Quest }            from './common/Quest';
+export { SearchAvatars }    from './common/SearchAvatars';
+export { Seeds }            from './common/Seeds';
+export { Settings }         from './common/Settings';
+export { StarField }        from './common/StarField';
+export { Wallet }           from './common/Wallet';
